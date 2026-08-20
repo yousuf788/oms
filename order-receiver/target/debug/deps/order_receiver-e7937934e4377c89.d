7da96a5809bf26e3c9f1fc7fd0a14b42bcb5cd58
Yousuf Hassan <108810587+yousuf788@users.noreply.github.com>
2026-08-20 14:45:34 +0530

    Delete order-receiver/target directory

--- order-receiver/target/debug/deps/order_receiver-e7937934e4377c89.d
+++
@@ -1,6 +0,0 @@
-/data/exchange/files/order-receiver/target/debug/deps/order_receiver-e7937934e4377c89.d: src/main.rs src/config.rs
-
-/data/exchange/files/order-receiver/target/debug/deps/order_receiver-e7937934e4377c89: src/main.rs src/config.rs
-
-src/main.rs:
-src/config.rs:
--- order-receiver/target/debug/order-receiver.d
+++
@@ -1,1 +0,0 @@
-/data/exchange/files/order-receiver/target/debug/order-receiver: /data/exchange/files/order-receiver/src/config.rs /data/exchange/files/order-receiver/src/main.rs
